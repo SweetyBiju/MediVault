4a0224da4b19379d620fd3dc82c989d4510134f9
yosobanta <150269628+yosobanta@users.noreply.github.com>
2025-07-31 23:01:57 +0530

    Updated insightRoutes.js
    routes for generating, fetching insights

--- Backend/routes/insightRoutes.js
+++ Backend/routes/insightRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getInsights } from '../controllers/insightController.js';
-import {protect} from '../middleware/auth.js';
+import { generateInsights, getInsights, getOverallInsights } from '../controllers/insightController.js';
+import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/', protect, getInsights);
+router.post('/generate', protect, generateInsights); // Generate + Save, no multer
+router.get('/:reportId', protect, getInsights); // Retrieve only
+router.get('/overall/:userId', protect, getOverallInsights);
 
 export default router;
