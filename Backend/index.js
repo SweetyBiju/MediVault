36f27104e4e8911d04c846d48fc094d638cfc12d
Sweety Biju <147310350+SweetyBiju@users.noreply.github.com>
2025-08-11 01:02:35 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -19,6 +19,7 @@ const app = express();
 // ===== Middleware =====
 const allowedOrigins = [
   'http://localhost:5173', // Local frontend
+  'https://medi-vault-ten.vercel.app/',
   'https://medivault-ebon.vercel.app' // Production frontend
 ];
 
@@ -59,3 +60,4 @@ app.use((err, req, res, next) => {
 // ===== Server Listen =====
 const PORT = config.port || 5600;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
